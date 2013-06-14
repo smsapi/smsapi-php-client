ba4e35d52edce15baddb38633a8126c0f62425bb
Sebastian <s.pondo@smsapi.pl>
2013-06-14 16:46:14 +0200

    normalizowane nazwy klas w autoloader

--- smsapi/Autoload.php
+++ smsapi/Autoload.php
@@ -4,25 +4,35 @@ spl_autoload_register(
 	function ($class) {
 		static $classes = null;
 		static $path = null;
-
+		
 		if ( $path === null ) {
 			$path = dirname( __FILE__ );
 		}
-
-		if ( !isset( $classes[ $class ] ) ) {
-
-			if ( preg_match( '/^SMSApi/', $class ) ) {
-
-				$class = explode( "\\", $class );
+		
+		$cname = strtolower(str_replace("\\", "", $class));
+		
+		if ( !isset( $classes[ $cname ] ) ) {
+			
+			if ( preg_match( '/^smsapi/', $cname ) ) {
+				
+				$classmap = include '_classmap.php';
+				
+				if(isset($classmap[$cname])){
+					$class = explode( "\\", $classmap[$cname] );
+				}else{
+					$class = explode( "\\", $class );
+				}
+				
 				unset( $class[ 0 ] );
 				$class = implode( DIRECTORY_SEPARATOR, $class );
 
-				$classes[ $class ] = $path . DIRECTORY_SEPARATOR . $class . '.php';
+				$classes[ $cname ] = $path . DIRECTORY_SEPARATOR . $class . '.php';
 			}
+					
 		}
 
-		if ( isset( $classes[ $class ] ) ) {
-			require $classes[ $class ];
+		if ( isset( $classes[ $cname ] ) ) {			
+			require $classes[ $cname ];
 		}
 	}
 );
