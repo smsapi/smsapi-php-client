41d318fe5dc07b5f0497494ac75aadd54b7b4f95
Marek Sudoł <m.sudol@smsapi.pl>
2015-07-01 08:32:57 +0200

    5.3 test fix

--- test/Integration/config.php
+++ test/Integration/config.php
@@ -5,12 +5,12 @@ $distFilePath = __DIR__ . '/config.dist.php';
 if (is_file($distFilePath)) {
     return include $distFilePath;
 } else {
-    return [
+    return array(
         'api_login' => '',
         'api_password' => '',
         'number_test' => '',
         'sms_template_name' => '',
         'host' => '',
-    ];
+    );
 }
 
