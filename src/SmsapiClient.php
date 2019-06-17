a5a3ad028e48d97b8e748774b7f649b568e0918c
Maciej Lew <maciej.lew.1987@gmail.com>
2019-06-17 12:04:12 +0200

    Fixes , resolves #84

--- src/SmsapiClient.php
+++ src/SmsapiClient.php
@@ -12,7 +12,7 @@ use Smsapi\Client\Service\SmsapiPlService;
  */
 interface SmsapiClient extends LoggerAwareInterface
 {
-    const VERSION = '2.3.1';
+    const VERSION = '2.3.2';
 
     public function setProxy(string $proxy): self;
 
