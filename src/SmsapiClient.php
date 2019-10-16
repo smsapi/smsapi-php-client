5e7e361c989bda3855aa8278cb5afb4a16df7af2
Maciej Lew <maciej.lew.1987@gmail.com>
2019-10-16 08:47:50 +0200

    Releases version 2.3.4, resolves #85

--- src/SmsapiClient.php
+++ src/SmsapiClient.php
@@ -12,7 +12,7 @@ use Smsapi\Client\Service\SmsapiPlService;
  */
 interface SmsapiClient extends LoggerAwareInterface
 {
-    const VERSION = 'Unreleased';
+    const VERSION = '2.3.4';
 
     public function setProxy(string $proxy): self;
 
