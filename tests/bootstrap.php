f0517395c8dd7e051075a1171759c37a4df83b51
Maciej Lew <m.lew@smsapi.pl>
2020-10-08 08:42:28 +0200

    Fixes legacy test framework error reporting issues

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,26 @@
 <?php
 declare(strict_types=1);
 
+use PHPUnit\Util\Blacklist;
+
 define('ROOT_DIR', dirname(__DIR__));
 define('RESOURCE_DIR', ROOT_DIR . '/tests-resources');
 define('CONFIG_DIR', RESOURCE_DIR . '/config');
 define('FIXTURES_DIR', RESOURCE_DIR . '/fixtures');
 
 require_once ROOT_DIR . '/vendor/autoload.php';
+
+/**
+ * @see https://github.com/sebastianbergmann/phpunit/issues/1598#issuecomment-434340706
+ */
+if (!empty(Blacklist::$blacklistedClassNames)) {
+    foreach (Blacklist::$blacklistedClassNames as $className => $parent) {
+        try {
+            if (!class_exists($className)) {
+                unset(Blacklist::$blacklistedClassNames[$className]);
+            }
+        } catch (Exception $e) {
+            unset(Blacklist::$blacklistedClassNames[$className]);
+        }
+    }
+}
