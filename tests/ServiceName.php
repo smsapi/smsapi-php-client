e8524a0d2ce28eb18a422c054a3ea6876fe90dea
Maciej Lew <maciej.lew.1987@gmail.com>
2021-01-27 11:51:08 +0100

    Removes php-enum from dev dependencies.

--- tests/ServiceName.php
+++ tests/ServiceName.php
@@ -3,14 +3,7 @@ declare(strict_types=1);
 
 namespace Smsapi\Client\Tests;
 
-use MabeEnum\Enum;
-
-/**
- * @method static ServiceName SMSAPI_PL
- * @method static ServiceName SMSAPI_COM
- * @method static ServiceName byValue($value)
- */
-class ServiceName extends Enum
+class ServiceName
 {
     const SMSAPI_PL = 'SMSAPI.PL';
     const SMSAPI_COM = 'SMSAPI.COM';
